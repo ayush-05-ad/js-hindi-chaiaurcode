c3307c95e12a36f53101a5739899fbe2903b09dc
Ayush Deep <76211201+ayush-05-ad@users.noreply.github.com>
2026-07-21 14:02:58 +0000

    Data Types and Variables

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict" //treate all js code as newer version
+
+//alert("hello")
+//alert(3+3) //o/p: error => not defined in node compiler but console 
+//it give pop in browser and give value (6)
+//At the time we not use this, there is other method to use alert
+
+//we are using node JS not browser 
+//To read mdn for this 
+//for ecma original document of JavaScript
+
+let name = "Ayush" //String
+let age = 18 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+
+//dataType
+//number => 2 to power 5
+//bigint (use for big value number basically use in tradingWebsite, Facebook etc)
+//String => ""
+//boolean => true/false
+//null => Standalone value represent of empty value
+//undefined => Value not declared
+//Symbol => use in figma, react (unique) ka lia
+
+//Object
+
+console.log(typeof "Ayush") //o/p:String
+console.log(typeof age) //o/p: number
+console.log(typeof null);//o/p: object
+console.log(typeof undefined); //o/p: undefined
+
+//Read ECMA Script => Specification
